f59f3fcd97d27ce949c88f04eea58125ce0f1a00
clatour <chandler.latour@gmail.com>
2021-10-27 10:09:26 -0500

    Initial use-ruby-install

--- lib/utils.sh
+++ lib/utils.sh
@@ -1,5 +1,5 @@
-RUBY_BUILD_VERSION="${ASDF_RUBY_BUILD_VERSION:-v20211124}"
-RUBY_BUILD_TAG="$RUBY_BUILD_VERSION"
+RUBY_INSTALL_VERSION="${ASDF_RUBY_INSTALL_VERSION:-v0.8.3}"
+RUBY_INSTALL_TAG="$RUBY_INSTALL_VERSION"
 
 echoerr() {
   >&2 echo -e "\033[0;31m$1\033[0m"
@@ -9,46 +9,47 @@ errorexit() {
   echoerr "$1"
   exit 1
 }
-
-ensure_ruby_build_setup() {
-  ensure_ruby_build_installed
+t
+ensure_ruby_install_setup() {
+  ensure_ruby_install_installed
 }
 
-ensure_ruby_build_installed() {
-    local ruby_build_version
+ensure_ruby_install_installed() {
+  local ruby_install_version
 
-    if [ ! -f "$(ruby_build_path)" ]; then
-        download_ruby_build
-    else
-        current_ruby_build_version="$("$(ruby_build_path)" --version | cut -d ' ' -f2)"
-        # If ruby-build version does not start with 'v',
-        # add 'v' to beginning of version
-        if [ ${current_ruby_build_version:0:1} != "v" ]; then
-          current_ruby_build_version="v$current_ruby_build_version"
-        fi
-        if [ "$current_ruby_build_version" != "$RUBY_BUILD_VERSION" ]; then
-            # If the ruby-build directory already exists and the version does not
-            # match, remove it and download the correct version
-            rm -rf "$(ruby_build_dir)"
-            download_ruby_build
-        fi
+  if [ ! -f "$(ruby_install_path)" ]; then
+    download_ruby_install
+  else
+    current_ruby_install_version="$("$(ruby_install_path)" --version | cut -d ' ' -f2)"
+    # If ruby-build version does not start with 'v',
+    # add 'v' to beginning of version
+    if [ ${current_ruby_install_version:0:1} != "v" ]; then
+      current_ruby_install_version="v$current_ruby_install_version"
     fi
+    if [ "$current_ruby_install_version" != "$RUBY_INSTALL_VERSION" ]; then
+      # If the ruby-build directory already exists and the version does not
+      # match, remove it and download the correct version
+      rm -rf "$(ruby_install_dir)"
+      download_ruby_install
+    fi
+  fi
+
 }
 
-download_ruby_build() {
+download_ruby_install() {
     # Print to stderr so asdf doesn't assume this string is a list of versions
-    echoerr "Downloading ruby-build..."
-    local build_dir="$(ruby_build_source_dir)"
+    echoerr "Downloading ruby-install..."
+    local build_dir="$(ruby_install_source_dir)"
 
     # Remove directory in case it still exists from last download
     rm -rf $build_dir
 
     # Clone down and checkout the correct ruby-build version
-    git clone https://github.com/rbenv/ruby-build.git $build_dir >&2 >/dev/null
-    (cd $build_dir; git checkout $RUBY_BUILD_TAG >&2 >/dev/null)
+    git clone https://github.com/postmodern/ruby-install.git $build_dir >&2 >/dev/null
+    (cd $build_dir; git checkout $RUBY_INSTALL_TAG >&2 >/dev/null)
 
     # Install in the ruby-build dir
-    PREFIX="$(ruby_build_dir)" $build_dir/install.sh
+    (cd $build_dir; PREFIX="$(ruby_install_dir)" make install)
 
     # Remove ruby-build source dir
     rm -rf $build_dir
@@ -57,14 +58,15 @@ download_ruby_build() {
 asdf_ruby_plugin_path() {
     echo "$(dirname "$(dirname "$0")")"
 }
-ruby_build_dir() {
-    echo "$(asdf_ruby_plugin_path)/ruby-build"
+
+ruby_install_dir() {
+    echo "$(asdf_ruby_plugin_path)/ruby-install"
 }
 
-ruby_build_source_dir() {
-    echo "$(asdf_ruby_plugin_path)/ruby-build-source"
+ruby_install_source_dir() {
+    echo "$(asdf_ruby_plugin_path)/ruby-install-source"
 }
 
-ruby_build_path() {
-    echo "$(ruby_build_dir)/bin/ruby-build"
+ruby_install_path() {
+    echo "$(ruby_install_dir)/bin/ruby-install"
 }
